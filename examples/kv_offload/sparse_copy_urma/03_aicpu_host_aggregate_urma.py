b41350c30d2f0e8a4cdbe7579d31dcc6d77eff3b
yangqi12138 <yangqi124@h-partners.com>
2026-09-14 15:59:01 +0800

    Reapply "fix: avoid AICPU scatter sync cache race"
    This reverts commit 46627be39da74a3b9f513814a1aefd02a0fe7ab7.

--- examples/kv_offload/sparse_copy_urma/03_aicpu_host_aggregate_urma.py
+++ examples/kv_offload/sparse_copy_urma/03_aicpu_host_aggregate_urma.py
@@ -63,7 +63,7 @@ class Timing(ctypes.Structure):
 
 
 CONTROL_STORAGE_BYTES = 2 * 4096 + ctypes.sizeof(Timing)
-PACKED_CONTROL_COPY_BYTES = CONTROL_STORAGE_BYTES
+PACKED_CONTROL_COPY_BYTES = 4096 + 64
 
 
 class CpuAffinityError(ValueError):
@@ -410,7 +410,6 @@ def make_device_control(request):
 
 
 def stage_device_control(handle, bm, control, hbm_gva):
-    ctypes.memset(ctypes.addressof(control) + 8192, 0, ctypes.sizeof(Timing))
     copy_to_hbm(handle, bm, ctypes.addressof(control), hbm_gva, PACKED_CONTROL_COPY_BYTES)
 
 
@@ -471,6 +470,7 @@ def run_npu(args, handle, bm, runtime_device, layout):
         launch = library.AccOffloadAggregateUrmaDemo
         launch.argtypes = [ctypes.c_uint64] * 5 + [ctypes.c_uint16]
         launch.restype = ctypes.c_int32
+        copy_to_hbm(handle, bm, ctypes.addressof(timing), hbm_gva + 8192, ctypes.sizeof(timing))
         for round_index in range(total_iterations(args)):
             if args.verify:
                 fill_destination_poison(ctypes.addressof(poison), stride, args.segments, args.segment_bytes)
